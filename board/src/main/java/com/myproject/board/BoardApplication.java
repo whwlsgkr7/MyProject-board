13312916cf9d026d881f4bb6cb37b5762c2ccbf9
jinhack <whwlsgkr7@naver.com>
2024-01-19 18:44:49 +0900

    #11 - 변경된 파일

--- board/src/main/java/com/myproject/board/BoardApplication.java
+++ board/src/main/java/com/myproject/board/BoardApplication.java
@@ -2,11 +2,14 @@ package com.myproject.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
 
 @SpringBootApplication
 public class BoardApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(BoardApplication.class, args);
     }
 
